0cac21b7d619e27c3efab494ce462513ebd10a22
Ryan Kooy <rwkooy@gmail.com>
2019-09-21 11:06:08 -0400

    test

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = process.env.PORT || 2940;
+// var PORT = process.env.PORT || 2940;
+var PORT = 2940;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
